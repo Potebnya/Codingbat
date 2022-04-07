0a4cb18d01713895333c43c9bca1b4a030e39c2c
Роман Потебня <103039218+Potebnya@users.noreply.github.com>
2022-04-07 19:46:44 +0300

    Add files via upload

---
+++ sumDouble.java
@@ -0,0 +1,19 @@
+package com.potebnya.StartJava.CodainBat.WarmUp1;
+
+//Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+//
+//
+//        sumDouble(1, 2) → 3
+//        sumDouble(3, 2) → 5
+//        sumDouble(2, 2) → 8
+
+public class sumDouble {
+    public int sumDouble(int a, int b) {
+        int sum = a +b;
+        if(a == b){
+            return sum * 2;
+        }
+        return sum;
+    }
+
+}
