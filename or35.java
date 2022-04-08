043fe63c3716df0c319bd2de30234e93047e26ec
Роман Потебня <103039218+Potebnya@users.noreply.github.com>
2022-04-08 16:52:22 +0300

    Add files via upload

---
+++ or35.java
@@ -0,0 +1,20 @@
+package com.potebnya.StartJava.CodainBat.WarmUp1;
+
+//Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+//
+//
+//        or35(3) → true
+//        or35(10) → true
+//        or35(8) → false
+public class or35 {
+    public boolean or35(int n) {
+        if( n % 3  == 0){
+            return true;
+        }
+        else if (n % 5 == 0){
+            return true;
+        }
+        return false;
+    }
+
+}
